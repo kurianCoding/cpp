e08f74849cb3d7d9892b3bde2ad134a059eb9378
kurian <kurianck.mail@gmail.com>
2016-11-06 22:02:08 +0530

    TIMEMARK
    completed:removed unwanted scripts
    next:some new code

--- runscript.sh
+++
@@ -1,5 +0,0 @@
-docker rm -f kurian-cpp-dev
-docker run  --name kurian-cpp-dev -it \
--v $PWD/code:/home \
--v $PWD/vim:/.vim \
-$1 /bin/bash 
--- startscript.sh
+++
@@ -1,3 +0,0 @@
-docker start kurian-cpp-dev
-#docker exec -ti kurian-python-dev script /dev/null -c 'bash -ilc tmux'
-docker exec -ti kurian-cpp-dev script -q -c "/bin/bash" /dev/null
