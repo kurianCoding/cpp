d618dd463c4961568bc1a2d0583f0f0bf24c6d66
kurian <kurianck.mail@gmail.com>
2017-03-03 08:05:23 +0530

    Makefile
    added makefile to compile and store the output executable in PATH.
    this will make the directory itself clean and allow to execute
    programs from command line like any normal program.

--- BFS.cpp
+++ BFS.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 class Graph {
     int V;//number of vertices of the graph
-    list <int> *adj;//pointer to an array containing ad
+    list<int> *adj;//pointer to an array containing ad
 public:
     Graph(int v);//consturctor funtion
     void addEdge(int v ,int w);//function to add an edge to the graph
@@ -27,10 +27,10 @@ void Graph::BFS(int s) {
 
     // queue will store the nodes which we need
     // to visit
-    list <int> queue;
+    list<int> queue;
     // this iterator can be used to loop over the entites of
     // queue
-    list <int> ::iterator i;
+    list<int> ::iterator i;
 
     queue.push_back(s);
     visited[s]=true;
@@ -44,9 +44,9 @@ void Graph::BFS(int s) {
         for(i=adj[s].begin(); i!=adj[s].end(); i++) {
             // if node is not visited put it in
             // the queue
-            if(!visited[i]) {
-                visited[i]=true;
-                queue.push_back(i);
+            if(!visited[*i]) {
+                visited[*i]=true;
+                queue.push_back(*i);
             }
         }
     }
---
+++ Makefile
@@ -0,0 +1,7 @@
+CC=g++
+CFLAGS=-Wall -g
+ODIR=/usr/local/bin
+.cpp:
+	$(CC) $@.cpp $(CFLAGS) -o /usr/local/bin/$@
+clean:
+	rm -f hello
