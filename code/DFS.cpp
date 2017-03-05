18913bc2a927b9cd448229fbad8f18ccc6b1b10e
kurian <kurianck.mail@gmail.com>
2017-03-05 18:30:25 +0530

    Depth First Algorithm
    Graph: the creator function simply takes in the number of nodes.
    it initializes the adj list which is an array of lists which cont
    ain adjacent nodes of each node.
    add Edge: It takes in two integers, it will add the second node as
    the adjacent node of the first node.
    
    DFSUtil is a a code simplification of DFS function. DFS util takes in
    the visited array, and the node to be visited(or checked). It then
    visits that node and checks its adjacent nodes by calling the DFSUtil
    on the visited array and the node number.
    
    DFS function is the fairly simple function which initialises a visite
    d array and passes it into DFSUtil function.

--- code/DFS.cpp
+++ code/DFS.cpp
@@ -10,3 +10,47 @@
  * is removed from the stack, else the first unvisited
  * adjacent node is visited by calling the function*/
 #include <iostream>
+#include <list>
+class Graph {
+    int V;
+    std::list<int> *adj;
+public:
+    Graph(int w);
+    void addEdge(int u,int v);
+    void DFSUtil(int v,bool visited[]);
+    void DFS(int v);
+};
+Graph::Graph(int w) {
+    this->V=w;
+    adj=new std::list<int>[w];
+}
+void Graph::addEdge(int u, int w) {
+    adj[u].push_back(w);
+}
+void Graph::DFSUtil(int v, bool visited[]) {
+    visited[v]=true;
+    std::cout<<v<<" ";
+    std::list<int>::iterator i;
+    for(i=adj[v].begin(); i!=adj[v].end(); ++i) {
+        if(!visited[*i]) {
+            DFSUtil(*i,visited);
+        }
+    }
+}
+void Graph::DFS(int v) {
+    bool *visited=new bool[V];
+    for(int i=0; i<V; i++) {
+        visited[i]=false;
+    }
+    DFSUtil(v,visited);
+}
+int main() {
+    Graph g(4);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,2);
+    g.addEdge(2,0);
+    g.addEdge(2,3);
+    g.addEdge(3,3);
+    g.DFS(2);
+}
