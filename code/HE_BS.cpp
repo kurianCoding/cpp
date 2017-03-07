0ebf3755dc756dde6e82e4045831d50272af0c63
kurian <kurianck.mail@gmail.com>
2017-03-07 20:45:39 +0530

    bishnu and power in hacker earth
    a simple dynamic array problem involving, an array to store the
    power of all the candidates and a function to count the number
    of candidates which are less than the given strength.
    
    adding a file which does binary search of sorted array.

---
+++ code/HE_BS.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int getPoints(int array[],int length, int strength,bool countFlag) {
+    int sum=0,count=0;
+    for(int j=0; j<length; j++) {
+        if(strength>=array[j]) {
+            sum=sum+array[j];
+            count++;
+        }
+    }
+    if(countFlag) {
+        return count;
+    }
+    return sum;
+}
+int main() {
+    int j,sol,rounds,count;
+    cin>>sol;
+    int power[sol];
+    for(j=0; j<sol; j++) {
+        cin>>power[j];
+    }
+    cin>>rounds;
+    int currentStrength=0,points=0;
+    for(j=0; j<rounds; j++) {
+        cin>>currentStrength;
+        points=getPoints(power,sol,currentStrength,false);
+        count=getPoints(power,sol,currentStrength,true);
+        cout<<count<<" "<<points<<endl;
+    }
+    return 0;
+}
---
+++ code/binarySearch.cpp
@@ -0,0 +1,12 @@
+/* this is a program to demonstarte the working
+ * of binary search. It will take in a sorted a
+ * rray and take n queries. for each of the
+ * queries it will return the number of steps
+ * it required to arrive at the index and the ind
+ * ex.*/
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main() {
+
+}
