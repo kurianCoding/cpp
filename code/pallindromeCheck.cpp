1bf93e4ce9513d1f4956f397378622383831cd0d
kurian <kurianck.mail@gmail.com>
2017-04-04 07:51:54 +0530

    check pallindrome

---
+++ code/pallindromeCheck.cpp
@@ -0,0 +1,10 @@
+/* pallindromes are strings which read the same forwards and
+ * backwards. the challenege here is to pick pallindromes from
+ * a series of lucky strings.*/
+#include <iostream>
+using namespace std;
+int main() {
+	int tests;
+	cin >> tests;
+	return 0;
+}
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 docker rm -f cppDev
 docker run --name cppDev \
 	-ti \
+	--hostname="cpp-dev"\
 	-v $PWD/../code:/home/code \
 	--env-file=$PWD/../conf/cpp.env \
 	 $1 /bin/bash
