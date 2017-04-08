a71aaf83ad5f9b026cce5b81df084954582b06f0
kurian <kurianck.mail@gmail.com>
2017-04-08 15:02:42 +0530

    pallindrome check
    added the function which checks if the given string is a pallindrome.

--- code/bennySegments.cpp
+++ code/bennySegments.cpp
@@ -1,7 +1,31 @@
 /* a small program to
  * */
 #include <iostream>
+bool isPrime(int k, int* array, int j) {
+	for (int u = 0; u < j; u++) {
+		if (array[k] != 0) {
+			if (array[u] % k == 0) {
+				return false;
+			} else {
+				if (u == j) {
+					return true;
+				}
+			}
+		}
+	}
+}
 int main() {
-	std::cout << "hello";
+	int primeNumbers[10];
+	int k = 0, j = 0;
+	while (k < 10) {
+		if (isPrime(k, primeNumbers, j)) {
+			primeNumbers[j] = k;
+			j++;
+		}
+		k++;
+	}
+	for (int i = 0; i < 10; i++) {
+		std::cout << primeNumbers[i] << std::endl;
+	}
 	return 0;
 }
