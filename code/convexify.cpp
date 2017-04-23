5006d34b61dcf07d4ba5c26cd0880aa3eeffe0ad
kurian <kurianck.mail@gmail.com>
2017-04-23 21:53:15 +0530

    convexify
    a simple program to output a convex set from a given set of points

---
+++ code/convexify.cpp
@@ -0,0 +1,8 @@
+/* this is a program to pick from the given in put
+ * set of points a set which is a convex hull. this
+ * means, the boundary of the set of points is formed
+ * by a convex polygon*/
+
+#include <iostream>
+using namespace std;
+int main() { return 0; }
