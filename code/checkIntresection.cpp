d738aff4f6cede287f58ab14b54a4bc6d360c155
kurian <kurianck.mail@gmail.com>
2017-04-27 06:57:53 +0530

    line intersection
    a program to see if the two line segments intersect or not. It relies
    on an algorithm to see if the points are in a clockwise or counter
    clockwise manner.

---
+++ code/checkIntresection.cpp
@@ -0,0 +1,16 @@
+/* this is a program to chaeck if two line segments
+ * represented by 4 points intersect or not
+ */
+#include <iostream>
+using namespace std;
+struct Segment {
+	int originx, originy;
+	int endx, endy;
+};
+/*this function checks if the angle is clockwise or counter clockwise
+ * */
+bool doIntersect(Segment* a, Segment* b) {
+	int angle = (a->originx - a->endx) * (b->originy - b->endy) /
+		    ((a->originy - a->endy) * (b->originx - b->endx));
+	return angle > 0;
+}
