64e3afae3701dd3f3b5bde7a680e3ebb0fb1efa8
kurian <kurianck.mail@gmail.com>
2017-06-08 06:56:55 +0530

    Geeks for Geeks Question:Largest Sub Array
    Given an array and a number k, find the largest sum of the subarray
    containing at least k numbers. It may be assumed that the size of
    array is at-least k.

---
+++ code/subArraySum.cpp
@@ -0,0 +1,13 @@
+/* Given an array and a number k, find the largest sum of the subarray containing at least k numbers. It may be assumed that the size of array is at-least k. */
+
+/* Input:​ */
+/* The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains an integer n denoting the size of the array. Then the following line contains n space separated integers. The last line of the input contains the number k. */
+
+/* Output: */
+/* Print the value of the largest sum of the subarray containing at least k numbers. */
+
+/* Constraints: */
+/* 1<=T<=10^5 */
+/* 1<=n<=10^5 */
+/* 1<=a[i]<=10^5 */
+/* 1<=k<=n */
